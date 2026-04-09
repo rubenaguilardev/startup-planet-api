d3a047f995d6efb7f4aade5c3712afe870a68a6e
Ruben Aguilar <ragui56@wgu.edu>
2026-04-09 02:03:26 -0700

    add data

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import { startups } from './data/data.js'
 
 const PORT = 8000
 
 const app = express()
+
+app.get('/api', (req, res) => res.json(startups))
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
